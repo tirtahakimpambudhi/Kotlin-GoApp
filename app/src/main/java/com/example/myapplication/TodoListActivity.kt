27eeb93e618d60e10beac0b272f42275dc40ef71
tirtahakimpambudhi <awebthp@gmail.com>
2025-02-08 13:48:14 +0700

    feat(layout): :iphone: Update layout in file TodoListActivity.kt chore: Update TodoListActivity.kt files

--- app/src/main/java/com/example/myapplication/TodoListActivity.kt
+++ app/src/main/java/com/example/myapplication/TodoListActivity.kt
@@ -1,10 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.adapter.TodoAdapter
