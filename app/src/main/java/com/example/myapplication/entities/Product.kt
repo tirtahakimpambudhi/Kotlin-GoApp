499c3c3270f9562b65850b4d46b14e607d4ee425
tirtahakimpambudhi <awebthp@gmail.com>
2025-02-28 11:03:39 +0700

    feat(entities): :sparkles: Update entities in the file Product.kt

--- app/src/main/java/com/example/myapplication/entities/Product.kt
+++ app/src/main/java/com/example/myapplication/entities/Product.kt
@@ -6,12 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "products")
 data class Product(
-    @PrimaryKey(autoGenerate = true) val productId: Int,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "price") val price: Double,
-    @ColumnInfo(name = "image") val image: String,
-    @ColumnInfo(name = "description") val description: String?,
-    @ColumnInfo(name = "created_at") val createdAt: Long,
-    @ColumnInfo(name = "modified_at") val modifiedAt: Long
+    @PrimaryKey(autoGenerate = true) var productId: Int,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "price") var price: Double,
+    @ColumnInfo(name = "stock") var stock: Int,
+    @ColumnInfo(name = "image") var image: String,
+    @ColumnInfo(name = "description") var description: String?,
+    @ColumnInfo(name = "created_at") var createdAt: Long,
+    @ColumnInfo(name = "modified_at") var modifiedAt: Long
 )
 
