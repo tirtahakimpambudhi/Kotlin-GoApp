589188158d19f6aa9b9e5f9fac601973747c1ff7
tirtahakimpambudhi <awebthp@gmail.com>
2025-03-01 11:03:40 +0700

    feat(entities): :sparkles: Update entities in the file ProductCategoryCrossRef.kt

--- app/src/main/java/com/example/myapplication/entities/ProductCategoryCrossRef.kt
+++ app/src/main/java/com/example/myapplication/entities/ProductCategoryCrossRef.kt
@@ -4,6 +4,6 @@ import androidx.room.Entity
 
 @Entity(primaryKeys = ["productId","categoryId"], tableName = "products_categories_cross_ref")
 data class ProductCategoryCrossRef(
-    val productId  :Int,
-    val categoryId :Int
+    var productId  :Int,
+    var categoryId :Int
 )
