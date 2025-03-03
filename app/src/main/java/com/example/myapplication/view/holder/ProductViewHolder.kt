a679a936482254c757f410a0d729529f380a5776
tirtahakimpambudhi <awebthp@gmail.com>
2025-03-03 11:03:42 +0700

    feat(holder): :sparkles: Update holder in the file ProductViewHolder.kt

--- app/src/main/java/com/example/myapplication/view/holder/ProductViewHolder.kt
+++ app/src/main/java/com/example/myapplication/view/holder/ProductViewHolder.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication.view.holder
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ProductItemBinding
 import com.example.myapplication.entities.Product
@@ -14,7 +16,25 @@ class ProductViewHolder(
 ): RecyclerView.ViewHolder(binding.root) {
     private val timeFormat = DateTimeFormatter.ofPattern("HH:mm")
 
-    fun bindTodo(item: Product) {
+    fun bindProduct(item: Product) {
+        binding.tvProductName.text = item.name
+        binding.tvPrice.text = item.price.toString()
+        binding.tvStock.text = item.stock.toString()
+        binding.ivProduct.setImageBitmap(BitmapFactory.decodeFile(item.image))
 
+        binding.menuButton.setOnClickListener {
+            val dialog = AlertDialog.Builder(context)
+            dialog.setTitle("Product Action")
+            dialog.setMessage("Do you want to proceed with this action?")
+
+            dialog.setPositiveButton("Edit") { dialog, _ ->
+                clickListener.editProductItem(item)
+            }
+
+            dialog.setNegativeButton("Delete") { dialog, _ ->
+                clickListener.deleteProductItem(item)
+            }
+            dialog.create().show()
+        }
     }
 }
