6dc51998596f3ccda281086ae6b3e898b19130d5
tirtahakimpambudhi <awebthp@gmail.com>
2025-03-05 11:03:44 +0700

    feat(model): :sparkles: Update model in the file ProductViewModel.kt

--- app/src/main/java/com/example/myapplication/view/model/ProductViewModel.kt
+++ app/src/main/java/com/example/myapplication/view/model/ProductViewModel.kt
@@ -3,9 +3,11 @@ package com.example.myapplication.view.model
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.entities.Product
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class ProductViewModel : ViewModel() {
-     var productItems = MutableLiveData<MutableList<Product>?>()
+    var productItems = MutableLiveData<MutableList<Product>?>()
 
     init {
         productItems.value = mutableListOf()
@@ -18,13 +20,24 @@ class ProductViewModel : ViewModel() {
     }
 
 
-    fun deleteTodoItem(id: Int)
+    fun updateProductItem(productId: Int, item: Product ) {
+        val list = productItems.value
+        val productItem = list!!.find { it.productId == productId }!!
+        productItem.name = item.name
+        productItem.image = item.image
+        productItem.price = item.price
+        productItem.description = item.description
+        productItem.stock = item.stock
+        productItem.modifiedAt = System.currentTimeMillis()
+        productItems.postValue(list)
+    }
+
+
+    fun deleteProduct(id: Int)
     {
         val list = productItems.value
         val todo = list!!.find { it.productId == id }!!
         list.remove(todo)
         productItems.postValue(list)
     }
-
-
 }
